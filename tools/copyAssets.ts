c9b1c92df9292ea82551accb3d36c0da6bad0eca
suvedhs <suvedhs@gmail.com>
2022-02-24 23:05:31 -0600

    added react

--- tools/copyAssets.ts
+++ tools/copyAssets.ts
@@ -1,6 +1,6 @@
 import * as shell from "shelljs";
 // Copy all the view templates and assets in the public folder
-shell.cp("-R", ["src/views", "src/public"], "app/");
+shell.cp("-R", ["src/views", "src/public"], "dist/");
 
 // Remove unnecessary files
-shell.rm(["app/public/js/*.ts", "app/public/js/*.json"]);
+shell.rm(["dist/public/js/*.ts", "dist/public/js/*.json"]);
